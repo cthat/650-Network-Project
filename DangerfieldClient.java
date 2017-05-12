4a9bd6955bca585d271cc395360d61a066ca3376
Will Smith <wsmith@researchinnovations.com>
2017-05-12 17:05:43 -0400

    In-progress update. - Fixed line tabs for better code readability - Added TODOs to highlight remaining work needed - Moved some variable declarations to top of classes to make code easier to read - Moved the section where we connect to port 23152 into a method so we can call that again without repeating code later in the program

--- DangerfieldClient.java
+++ DangerfieldClient.java
@@ -4,68 +4,124 @@ import java.io.*;
 
 public class DangerfieldClient {
 	
-	@SuppressWarnings("resource")
-	public static void main(String[] args) throws Exception {
-		
-		int psize, tout, byteSize, tcpPort = 23151, udpPort = 23152;
-		String url;
-		Socket s;
-		InetAddress sAddr;
-		DatagramSocket ds;
-		DatagramPacket dp;
-		Scanner scanner;
-		PrintWriter out;
-		BufferedReader br;
-		byte[] recvData;
-		boolean receiving;
-				
-		/*
-		 * Get arguments: psize tout url
-		 */
-		scanner = new Scanner(System.in);		
-		System.out.println("Enter: psize tout sname");
-		String line = scanner.nextLine();
-		String[] split = line.split("\\s+");
-		scanner.close();
-		psize = Integer.parseInt(split[0]);
-		tout = Integer.parseInt(split[1]);
-		url = split[2];
-				
-		/*
-		 * Establish socket
-		 */
-		sAddr = InetAddress.getByName("localhost");
-		s = new Socket(sAddr,tcpPort);
-				
-		/*
-		 * Send psize and url to server
-		 */
-		out = new PrintWriter(s.getOutputStream(), true);
-		out.println(psize);
-		out.println(url);
-						
-		/*
-		 * Get byteSize from server and send "size OK" message
-		 */
-		br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		byteSize = Integer.parseInt(br.readLine());
-		
-		System.out.println("Received: "+byteSize); // TESTING - Print out bytes received from server
-		out = new PrintWriter(s.getOutputStream(), true);
-		out.println("size OK");
-				
-		/* TODO: Set timeout to tout and wait to receive all packets from server over UDP 23152
-		ds = new DatagramSocket(udpPort);
-		recvData = new byte[psize];
-		dp = new DatagramPacket(recvData, recvData.length);
-						
-		receiving = true;
-		while (receiving) {
-			ds.receive(dp);			
-						
-		}
-		*/
-	}
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws Exception {
+
+        int psize, tout, byteSize;
+        int TCPPORT = 23151, UDPPORT = 23152;
+        //TODO: Can probably get rid of false assignment here
+        //variable to use as comparison for send/rec success
+        boolean success = false;
+        String url, line;
+        String [] split;
+        Socket s;
+        InetAddress sAddr;
+        DatagramSocket ds;
+        DatagramPacket dp;
+        Scanner scanner;
+        PrintWriter out;
+        BufferedReader br;
+        byte[] recvData;
+        boolean receiving;
+
+        /*
+         * Get arguments: psize tout url
+         */
+        //TODO: Need to add back in input validation. Need to convert tout input from sec to ms?
+        scanner = new Scanner(System.in);		
+        System.out.println("Enter: psize tout sname");
+        line = scanner.nextLine();
+        split = line.split("\\s+");
+        scanner.close();
+        psize = Integer.parseInt(split[0]);
+        tout = Integer.parseInt(split[1]);
+        url = split[2];
+
+        /*
+         * Establish socket
+         */
+        sAddr = InetAddress.getByName("localhost");
+        s = new Socket(sAddr,TCPPORT);
+
+        /*
+         * Send psize and url to server
+         */
+        out = new PrintWriter(s.getOutputStream(), true);
+        out.println(psize);
+        out.println(url);
+
+        /*
+         * Get byteSize from server and send "size OK" message
+         */
+        br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        byteSize = Integer.parseInt(br.readLine());
+
+        System.out.println("Received: "+byteSize); // TESTING - Print out bytes received from server
+        out = new PrintWriter(s.getOutputStream(), true);
+        out.println("size OK");
+
+        /* TODO: Set timeout to tout and wait to receive all packets from server over UDP 23152
+        ds = new DatagramSocket(udpPort);
+        recvData = new byte[psize];
+        dp = new DatagramPacket(recvData, recvData.length);
+
+        receiving = true;
+        while (receiving) {
+                ds.receive(dp);			
+
+        }
+        */
+        
+        // Client port 23152
+        connectToPort23152(psize, tout);
+        
+        //TODO: Need to compare rec'd page bytes to pageBytes.
+        
+        //TODO: If pageBytes matches, send single message page OK
+        
+        //TODO: Print the text in the page, in the correct order regardless of when packets arrived
+        
+        //TODO: Server needs to print IP address/OK when client rec'd everything correctly
+        
+        //TODO: If all bytes not rec'd before timeout, client sends message page fail to server
+        
+        //TODO: if server gets page fail msg, it retransmits all page bytes w/o another request to web server
+        if(success == false){
+            //doubling size of tout to retry connection
+            tout = tout * 2;
+            connectToPort23152(psize, tout);
+        }
+        //TODO: Will need to see if second time was successful, and if so, update success to true
+        
+        if(success == false){
+            System.out.println("\n quit");
+            //TODO: If fails again, server prints msg quit
+        }
+    }
+    
+    public static void connectToPort23152(int psize, int tout) throws IOException{
+        try {
+            DatagramSocket dsocket = new DatagramSocket(23152);
+            byte[] buffer = new byte[psize+1];
+            DatagramPacket dpacket = new DatagramPacket(buffer, buffer.length);
+            //dsocket.setSoTimeout(10000);
+            dsocket.setSoTimeout(tout);
+            while (true) {
+                try { // recieve data until timeout
+                    dsocket.receive(dpacket);
+                    Byte dd = buffer[dpacket.getLength()-1];
+                    System.out.println("Received packet " +dd.intValue());
+                    String packet = new String(buffer, 0, dpacket.getLength());
+                    dpacket.setLength(buffer.length);
+                } catch (SocketTimeoutException e) {
+                    System.out.println("Timeout reached!!! ");
+                    dsocket.close();
+                }
+            }
+        } catch (SocketException e1) {
+            System.out.println("Socket closed");
+        }
+    }
 }
 		
 	
--- DangerfieldServer.java
+++ DangerfieldServer.java
@@ -1,85 +1,121 @@
 import java.io.IOException;
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
 
 public class DangerfieldServer {
-	@SuppressWarnings("resource")
-	public static void main(String[] args) throws IOException {
-		int sPort = 23151;
-		boolean listening = true;
-	
-		ServerSocket ss = new ServerSocket(sPort);
-		
-		/*
-		 * Server listens for new clients
-		 * When they arrive they are accepted and a thread spawned to handle the work
-		 */
-		while(listening) {
-			new WorkerThread(ss.accept()).start();
-		}		
-	}	
-	
-	/* WorkerThread
-	 * 
-	 * Handles all the work with the client
-	 * 
-	 */
-	static class WorkerThread extends Thread {
-		private Socket s = null;
-		
-		public WorkerThread(Socket s) {
-			super("WorkerThread");
-			this.s = s;			
-		}
-		
-		public void run() {
-			int psize=0, byteSize;
-			String input=null;
-			BufferedReader br;
-			URL geturl;
-			PrintWriter out;
-		
-			try {
-				/*
-				 * Get psize and url from client
-				 */
-				br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-				psize = Integer.parseInt(br.readLine());
-				input = "http://"+br.readLine();
-				System.out.println("Received: "+psize+" "+input); 				// TESTING - prints arguments received from client
-				
-				/*
-				 * Get URL and send bytes received to client
-				 */
-				geturl = new URL(input);
-				br = new BufferedReader(new InputStreamReader(geturl.openStream()));
-				// TESTING - Prints the HTML received from the url
-				String line = null;
-				while ((line = br.readLine()) != null) {
-					System.out.println(line);				
-				}		
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws IOException {
+        int SPORT = 23151;
+        boolean listening = true;
 
-				// TODO - Send bytes received from webpage to client
-				// sends dummy value for now: 2 * psize + (psize/2)
-				byteSize = (2*psize)+(psize/2);
-				out = new PrintWriter(s.getOutputStream(), true);
-				out.println(byteSize);
-				
-				/*
-				 * Get "size OK" message for client
-				 */
-				br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-				input = br.readLine();
-				System.out.println(input); 				// TESTING - Prints out "size OK" from client
-				
-				// TODO - Send packets to client with bytes from webpage using UDP socket 23152
-				
-				s.close();
-				
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}						
-		}
-	}
+        ServerSocket ss = new ServerSocket(SPORT);
+
+        /*
+         * Server listens for new clients
+         * When they arrive they are accepted and a thread spawned to handle the work
+         */
+        while(listening) {
+            new WorkerThread(ss.accept()).start();
+        }		
+    }	
+
+    /* WorkerThread
+     * 
+     * Handles all the work with the client
+     * 
+     */
+    static class WorkerThread extends Thread {
+        private Socket s = null;
+
+        public WorkerThread(Socket s) {
+            super("WorkerThread");
+            this.s = s;			
+        }
+
+        public void run() {
+            int psize = 0, byteSize;
+            String input = null;
+            BufferedReader br;
+            URL sname;
+            PrintWriter out;
+
+            try {
+                /*
+                 * Get psize and url from client
+                 */
+                br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                psize = Integer.parseInt(br.readLine());
+                input = "http://"+br.readLine();
+                // TESTING - prints arguments received from client
+                System.out.println("Received: "+psize+" "+input); 				
+                
+                /*
+                 * Get URL and send bytes received to client
+                 */
+                sname = new URL(input);
+                br = new BufferedReader(new InputStreamReader(sname.openStream()));
+                // TESTING - Prints the HTML received from the url
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);				
+                }		
+                
+                URLConnection yc = sname.openConnection();
+
+                PrintWriter pw = new PrintWriter(s.getOutputStream());
+                pw.println(yc.getContentLength());
+                pw.flush();
+                BufferedReader read = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                String Response = read.readLine();
+                if (Response != null) {
+                    System.out.println("Client1 Says " + Response);
+                }
+ 
+                getBytes(psize, String.valueOf(sname), yc);
+                s.close();
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }						
+        }
+    }
+    
+    //Send UDP packets 23152
+    public static void getBytes(int psize, String sname, URLConnection urlConnection) throws IOException {
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        byte[] imgDataBa = new byte[urlConnection.getContentLength()];
+        DataInputStream dataIs = new DataInputStream(urlConnection.getInputStream());
+        dataIs.readFully(imgDataBa);
+        dataIs.close();
+        Integer count =0;
+      for (int i = 0; i < imgDataBa.length;) {
+          count++;
+         if (imgDataBa.length - i < psize) { //send last packet with remaining bytes
+            byte[] newByte = Arrays.copyOfRange(imgDataBa, i, imgDataBa.length+1);
+
+            newByte[newByte.length-1] = count.byteValue();
+
+            DatagramPacket packet = new DatagramPacket(newByte, newByte.length, address, 23152);
+            // Create a datagram socket, send the packet through it, close
+            // it.
+            DatagramSocket dsocket = new DatagramSocket();
+            dsocket.send(packet);
+            dsocket.close();
+         } else { // send packets with psize
+            byte[] newByte = Arrays.copyOfRange(imgDataBa, i, i + psize+1);
+
+                newByte[psize] 	 = count.byteValue();
+
+                DatagramPacket packet = new DatagramPacket(newByte, newByte.length, address, 23152);
+            // Create a datagram socket, send the packet through it, close it.
+            DatagramSocket dsocket = new DatagramSocket();
+            dsocket.send(packet);
+            dsocket.close();
+         }
+         i += psize;
+        }
+
+    }
 }
 			
