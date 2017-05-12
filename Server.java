99fa5e7155198642b2466f86725f166db3df3de2
Will Smith <wsmith@researchinnovations.com>
2017-05-12 15:06:39 -0400

    I was a dummy and committed old changes. Just reverting back to what master was before my mistake with this commit.

--- Server.java
+++ Server.java
@@ -4,75 +4,73 @@ import java.util.Arrays;
 
 public class Server {
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        ServerSocket ss = new ServerSocket(23151);
-        Socket s;
-        int psize;
-        String sname;
-        
-        System.out.println("Trying to establish client connection");
+		ServerSocket ss = new ServerSocket(23151);
 
-        s = ss.accept();
-        System.out.println("Connection established");
-        InputStreamReader ir = new InputStreamReader(s.getInputStream());
+		System.out.println("Trying to establish client connection");
 
-        BufferedReader br = new BufferedReader(ir);
-        psize = Integer.parseInt(br.readLine());
-        sname = br.readLine();
+		Socket s = ss.accept();
+		System.out.println("Connection established");
+		InputStreamReader ir = new InputStreamReader(s.getInputStream());
 
-        URL obj = new URL(sname);
+		BufferedReader br = new BufferedReader(ir);
+		int psize = Integer.parseInt(br.readLine());
+		String sname = br.readLine();
 
-        URLConnection yc = obj.openConnection();
+		URL obj = new URL(sname);
 
-        PrintWriter pw = new PrintWriter(s.getOutputStream());
-        pw.println(yc.getContentLength());
-        pw.flush();
-        BufferedReader read = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String Response = read.readLine();
-        if (Response != null) {
-            System.out.println("Client1 Says " + Response);
-        }
-        s.close();
-        ss.close();
-        //send UDP packets to port 
-        getBytes(psize, sname, yc);
+		URLConnection yc = obj.openConnection();
 
-    }
-    //Send UDP packets 23152
-    public static void getBytes(int psize, String sname, URLConnection urlConnection) throws IOException {
-        InetAddress address = InetAddress.getByName("127.0.0.1");
-        byte[] imgDataBa = new byte[urlConnection.getContentLength()];
-        DataInputStream dataIs = new DataInputStream(urlConnection.getInputStream());
-        dataIs.readFully(imgDataBa);
-        dataIs.close();
-        Integer count =0;
-      for (int i = 0; i < imgDataBa.length;) {
-          count++;
-         if (imgDataBa.length - i < psize) { //send last packet with remaining bytes
-            byte[] newByte = Arrays.copyOfRange(imgDataBa, i, imgDataBa.length+1);
+		PrintWriter pw = new PrintWriter(s.getOutputStream());
+		pw.println(yc.getContentLength());
+		pw.flush();
+		BufferedReader read = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		String Response = read.readLine();
+		if (Response != null) {
+			System.out.println("Client1 Says " + Response);
+		}
+		s.close();
+		ss.close();
+		//send UDP packets to port 
+		getBytes(psize, sname, yc);
 
-            newByte[newByte.length-1] = count.byteValue();
+	}
+	//Send UDP packets 23152
+	public static void getBytes(int psize, String sname, URLConnection urlConnection) throws IOException {
+		InetAddress address = InetAddress.getByName("127.0.0.1");
+		byte[] imgDataBa = new byte[urlConnection.getContentLength()];
+		DataInputStream dataIs = new DataInputStream(urlConnection.getInputStream());
+		dataIs.readFully(imgDataBa);
+		dataIs.close();
+		Integer count =0;
+	      for (int i = 0; i < imgDataBa.length;) {
+	          count++;
+	         if (imgDataBa.length - i < psize) { //send last packet with remaining bytes
+	            byte[] newByte = Arrays.copyOfRange(imgDataBa, i, imgDataBa.length+1);
 
-            DatagramPacket packet = new DatagramPacket(newByte, newByte.length, address, 23152);
-            // Create a datagram socket, send the packet through it, close
-            // it.
-            DatagramSocket dsocket = new DatagramSocket();
-            dsocket.send(packet);
-            dsocket.close();
-         } else { // send packets with psize
-            byte[] newByte = Arrays.copyOfRange(imgDataBa, i, i + psize+1);
+	            newByte[newByte.length-1] = count.byteValue();
 
-                newByte[psize] 	 = count.byteValue();
+	            DatagramPacket packet = new DatagramPacket(newByte, newByte.length, address, 23152);
+	            // Create a datagram socket, send the packet through it, close
+	            // it.
+	            DatagramSocket dsocket = new DatagramSocket();
+	            dsocket.send(packet);
+	            dsocket.close();
+	         } else { // send packets with psize
+	            byte[] newByte = Arrays.copyOfRange(imgDataBa, i, i + psize+1);
 
-                DatagramPacket packet = new DatagramPacket(newByte, newByte.length, address, 23152);
-            // Create a datagram socket, send the packet through it, close it.
-            DatagramSocket dsocket = new DatagramSocket();
-            dsocket.send(packet);
-            dsocket.close();
-         }
-         i += psize;
-        }
+	                newByte[psize] 	 = count.byteValue();
 
-    }
+	                DatagramPacket packet = new DatagramPacket(newByte, newByte.length, address, 23152);
+	            // Create a datagram socket, send the packet through it, close
+	            // it.
+	            DatagramSocket dsocket = new DatagramSocket();
+	            dsocket.send(packet);
+	            dsocket.close();
+	         }
+	         i += psize;
+		}
+
+	}
 }
